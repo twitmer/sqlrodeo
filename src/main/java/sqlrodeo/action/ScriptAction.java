39b7af0594232ba241e8e268bb6a74256604251d
Tom Witmer <github@tomwitmer.com>
2014-06-10 16:54:55 -0600

    Replace delegate with Script

---
+++ src/main/java/sqlrodeo/action/ScriptAction.java
@@ -0,0 +1,43 @@
+package sqlrodeo.action;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Node;
+
+import sqlrodeo.ExecutionContext;
+import sqlrodeo.implementation.JexlEvaluationException;
+import sqlrodeo.implementation.ValidationException;
+import sqlrodeo.util.StringUtils;
+
+public final class ScriptAction extends BaseAction {
+
+    Logger log = LoggerFactory.getLogger(ScriptAction.class);
+
+    public ScriptAction(Node node) {
+	super(node);
+    }
+
+    @Override
+    public void execute(ExecutionContext context)
+	    throws JexlEvaluationException {
+
+	String nodeText = getNodeText();
+
+	log.debug("Evaluating: " + nodeText);
+	Object result = context.evaluate(nodeText);
+	log.debug("result: "
+		+ result
+		+ (result != null ? ", class=" + result.getClass().getName()
+			: ""));
+
+    }
+
+    @Override
+    public void validate() {
+	String nodeText = getNodeText();
+	if (StringUtils.isEmpty(nodeText)) {
+	    throw new ValidationException(this, "No text in script node: "
+		    + toString());
+	}
+    }
+}
