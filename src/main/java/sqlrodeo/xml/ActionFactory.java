0258dcca62ea5d02574a6a0f12555dff87df2db6
Tom Witmer <github@tomwitmer.com>
2014-06-10 16:52:10 -0600

    Reorder elements, replace delegate with script.

--- src/main/java/sqlrodeo/xml/ActionFactory.java
+++ src/main/java/sqlrodeo/xml/ActionFactory.java
@@ -8,7 +8,6 @@ import sqlrodeo.Action;
 import sqlrodeo.action.AssignAction;
 import sqlrodeo.action.CommitAction;
 import sqlrodeo.action.ConnectionAction;
-import sqlrodeo.action.DelegateAction;
 import sqlrodeo.action.ExitAction;
 import sqlrodeo.action.IfAction;
 import sqlrodeo.action.IncludeAction;
@@ -16,6 +15,7 @@ import sqlrodeo.action.ObjectFactoryAction;
 import sqlrodeo.action.PropertiesAction;
 import sqlrodeo.action.QueryAction;
 import sqlrodeo.action.RollbackAction;
+import sqlrodeo.action.ScriptAction;
 import sqlrodeo.action.SqlAction;
 import sqlrodeo.action.SqlRodeoAction;
 import sqlrodeo.implementation.ValidationException;
@@ -32,32 +32,32 @@ final class ActionFactory {
 		    + "=%s", element));
 	}
 	switch (element.getNodeName()) {
-	case "objectFactory":
-	    return new ObjectFactoryAction(element);
-	case "if":
-	    return new IfAction(element);
-	case "connection":
-	    return new ConnectionAction(element);
-	case "commit":
-	    return new CommitAction(element);
-	case "rollback":
-	    return new RollbackAction(element);
-	case "properties":
-	    return new PropertiesAction(element);
-	case "exit":
-	    return new ExitAction(element);
-	case "query":
-	    return new QueryAction(element);
-	case "delegate":
-	    return new DelegateAction(element);
-	case "sql":
-	    return new SqlAction(element);
 	case "assign":
 	    return new AssignAction(element);
+	case "commit":
+	    return new CommitAction(element);
+	case "connection":
+	    return new ConnectionAction(element);
+	case "exit":
+	    return new ExitAction(element);
+	case "if":
+	    return new IfAction(element);
 	case "include":
 	    return new IncludeAction(element);
+	case "objectFactory":
+	    return new ObjectFactoryAction(element);
+	case "properties":
+	    return new PropertiesAction(element);
+	case "query":
+	    return new QueryAction(element);
+	case "rollback":
+	    return new RollbackAction(element);
 	case "sql-rodeo":
 	    return new SqlRodeoAction(element);
+	case "script":
+	    return new ScriptAction(element);
+	case "sql":
+	    return new SqlAction(element);
 	default:
 	    // Shouldn't be possible if XSD validation succeeded.
 	    NodeWrapper node = new NodeWrapper(element);
