61fb38bc0ab299858172428868c689fed4fbf41d
Tom Witmer <github@tomwitmer.com>
2014-06-11 16:55:27 -0600

    Refactor ObjectFactoryAction, and add a few comments.

--- src/main/java/sqlrodeo/action/ObjectFactoryAction.java
+++ src/main/java/sqlrodeo/action/ObjectFactoryAction.java
@@ -1,5 +1,6 @@
 package sqlrodeo.action;
 
+import java.io.IOException;
 import java.io.StringReader;
 import java.util.Hashtable;
 import java.util.Properties;
@@ -18,23 +19,44 @@ import org.slf4j.LoggerFactory;
 import org.w3c.dom.Node;
 
 import sqlrodeo.ExecutionContext;
+import sqlrodeo.implementation.JexlEvaluationException;
 import sqlrodeo.util.StringUtils;
 
+/**
+ * Action typically used for creating or obtaining a DataSource from JNDI.
+ */
 public final class ObjectFactoryAction extends BaseAction {
 
+    /** Logger */
     private static final Logger log = LoggerFactory.getLogger(ObjectFactoryAction.class);
 
+    /**
+     * Constructor.
+     * 
+     * @param node
+     */
     public ObjectFactoryAction(Node node) {
         super(node);
     }
 
-    private DataSource buildJndiConnection(String jndiName) {
+    /**
+     * Attempt to retrieve an object by name from JNDI.
+     * 
+     * @param jndiName Name of object within the JNDI context.
+     * @return The object, if found, otherwise null.
+     */
+    private Object retrieveJndiObject(String jndiName) {
+
+        if(jndiName == null || StringUtils.isEmpty(jndiName)) {
+            return null;
+        }
+
         try {
-            Context initCtx = new InitialContext();
-            Object jndiObject = initCtx.lookup("java:comp/env/" + jndiName);
+            Context initialContext = new InitialContext();
+            Object jndiObject = initialContext.lookup("java:comp/env/" + jndiName);
 
             if(jndiObject != null && jndiObject instanceof DataSource) {
-                return (DataSource)jndiObject;
+                return jndiObject;
             }
         } catch(NamingException e) {
             log.debug("Ignoring error from JNDI lookup: " + e.getMessage() + ", " + e.getClass().getName());
@@ -44,23 +66,50 @@ public final class ObjectFactoryAction extends BaseAction {
         return null;
     }
 
+    /*
+     * (non-Javadoc)
+     * @see sqlrodeo.Action#execute(sqlrodeo.ExecutionContext)
+     */
     @Override
     public void execute(ExecutionContext context) throws Exception {
 
-        DataSource dataSource = null;
-
+        // Attempt to retrieve the object from JNDI. If this succeeds, we can put it in the context and return immediately.
         String jndiName = context.substitute(getNode().getAttribute("name"));
-        if(!StringUtils.isEmpty(jndiName)) {
-            dataSource = buildJndiConnection(jndiName);
-            if(dataSource != null) {
-                log.debug("Datasource " + jndiName + " resolved.");
-                return;
-            }
+        Object theObject = retrieveJndiObject(jndiName);
+        if(theObject != null) {
+            log.debug("JNDI object named '" + jndiName + "' resolved to instance of " + theObject.getClass().getName());
+            context.put(getNode().getAttribute("id"), theObject);
+            return;
+        }
+
+        // Create and invoke an instance of the ObjectFactory.
+        ObjectFactory objectFactory = (ObjectFactory)Class.forName(context.substitute(getNode().getAttribute("factoryClassName"))).newInstance();
+        theObject = invokeObjectFactory(context, objectFactory);
+        log.debug("ObjectFactory created instance of " + theObject.getClass().getName());
+        context.put(getNode().getAttribute("id"), theObject);
+    }
+
+    /**
+     * Invoke the ObjectFactory.
+     * 
+     * @param context
+     * @param objectFactory
+     * @return The product of invoking the ObjectFactory's getObjectInstance() method.
+     * @throws JexlEvaluationException
+     * @throws IOException
+     * @throws NamingException
+     * @throws Exception
+     */
+    private Object invokeObjectFactory(ExecutionContext context, ObjectFactory objectFactory) throws JexlEvaluationException,
+            IOException, NamingException, Exception {
+
+        if(log.isDebugEnabled()) {
+            log.debug(String.format("invokeObjectFactory: context, objectFactory".replaceAll(", ", "=%s, ") + "=%s", context,
+                    objectFactory));
         }
 
         // Expand text properties, and store in the Reference.
         Properties props = new Properties();
-
         String text = context.substitute(getNode().getTextContent());
         if(text != null) {
             props.load(new StringReader(text));
@@ -71,30 +120,22 @@ public final class ObjectFactoryAction extends BaseAction {
             ref.add(new StringRefAddr(key, props.getProperty(key)));
         }
 
-        // Create an instance of the ObjectFactory.
-        ObjectFactory of = (ObjectFactory)Class.forName(context.substitute(getNode().getAttribute("factoryClassName"))).newInstance();
-
         // Define other variables we don't actually care about.
         InitialContext initialContext = new InitialContext();
         Name name = null;
         Hashtable<?, ?> env = new Hashtable<>();
 
         // Invoke the ObjectFactory to create the desired object.
-        Object product = of.getObjectInstance(ref, name, initialContext, env);
-
-        // TODO: There is no "close()" method on DataSources, so it'll be up to
-        // users of this library to correctly close a
-        // datasource, if that's needed.
-
-        context.put(getNode().getAttribute("id"), product);
+        Object theObject = objectFactory.getObjectInstance(ref, name, initialContext, env);
+        return theObject;
     }
 
-    /**
-     * Default implementation of validate(). Does nothing.
+    /*
+     * (non-Javadoc)
+     * @see sqlrodeo.Action#validate()
      */
     @Override
     public void validate() {
-        // Nothing to do here.
+        // Nothing to do. The XSD enforces things well enough.
     }
-
 }
