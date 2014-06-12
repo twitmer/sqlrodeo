6ee6117163b10ae1db65251b0fb7f0d6dea83412
Tom Witmer <github@tomwitmer.com>
2014-06-12 15:02:26 -0600

    Add comments.

--- src/main/java/sqlrodeo/action/SqlRodeoAction.java
+++ src/main/java/sqlrodeo/action/SqlRodeoAction.java
@@ -6,20 +6,37 @@ import org.w3c.dom.Node;
 
 import sqlrodeo.ExecutionContext;
 
+/**
+ * Action that implements the <sql-rodeo> element. This simply executes all of its children. ("Phrasing!")
+ */
 public final class SqlRodeoAction extends BaseAction {
 
+    /** Logger */
     Logger log = LoggerFactory.getLogger(SqlRodeoAction.class);
 
+    /**
+     * Constructor.
+     * 
+     * @param node
+     */
     public SqlRodeoAction(Node node) {
         super(node);
     }
 
+    /*
+     * (non-Javadoc)
+     * @see sqlrodeo.Action#execute(sqlrodeo.ExecutionContext)
+     */
     @Override
     public void execute(ExecutionContext context) {
         log.debug("execute(): " + toString());
         executeChildren(context);
     }
 
+    /*
+     * (non-Javadoc)
+     * @see sqlrodeo.Action#validate()
+     */
     @Override
     public void validate() {
     }
