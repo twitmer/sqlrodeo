34b9a495b618cc9e31a77953ee15f34d1e5557c0
Tom Witmer <github@tomwitmer.com>
2014-06-12 14:44:58 -0600

    Fix comments, restructure test a bit.

--- src/test/java/sqlrodeo/action/TestExitAction.java
+++ src/test/java/sqlrodeo/action/TestExitAction.java
@@ -13,32 +13,33 @@ import sqlrodeo.ExecutionContext;
 import sqlrodeo.implementation.ExecutionContextImplementation;
 
 /**
- * Test for the PropertiesAction class.
- * 
- * @see PropertiesAction
+ * Test for the ExitAction class.
  */
 public class TestExitAction {
 
     /**
-     * Test method for {@link sqlrodeo.action.ExitAction#execute(sqlrodeo.ExecutionContext)} .
+     * Happy path test method for {@link sqlrodeo.action.ExitAction#execute(sqlrodeo.ExecutionContext)}.
      * 
      * @throws Exception
      */
-    @Test
+    @Test(expected = ExitException.class)
     public void testExecute() throws Exception {
-        // Happy path, properties content.
+        // Given: An XML node for the action
         Element node = XmlFactory.build("<exit/>").getDocumentElement();
+
+        // Given: The ExitAction;
         Action action = new ExitAction(node);
 
+        // Expect: validation to succeed.
         action.validate();
 
+        // Given: the Execution context.
         ExecutionContext context = new ExecutionContextImplementation();
-        try {
-            action.execute(context);
 
-            fail("Expected to receive an ExitException");
-        } catch(ExitException e) {
-            // Happy path!
-        }
+        // When: ExitAction is executed, an ExitException is thrown.
+        action.execute(context);
+
+        // Expect: We never reach this line.
+        fail("Expected to receive an ExitException");
     }
 }
